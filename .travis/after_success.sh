53409b92a1751f8e056be4789cb4d93cc1679491
JC Carrillo <jccarrillo@acm.org>
2020-05-30 22:21:38 -0400

    push to master upon increasing the version

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -5,7 +5,7 @@ if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_COMMIT_MESSAGE" != "AUTO Versi
   mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.incrementalVersion} versions:commit
   git add .
   git commit -m "AUTO Version Increase"
-  git push
+  git push origin master
 elif [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_COMMIT_MESSAGE" == "AUTO Version Increase" ]; then
   echo 'deploying'
   mvn deploy --settings .travis/settings.xml
